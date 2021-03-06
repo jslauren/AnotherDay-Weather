2eb72a2824b75f6c07f3efdd8cb22c6d9ebc6e12
j.s.lauren <zzzbjs@naver.com>
2021-03-06 22:44:34 +0900

    Initial Commit

---
+++ AnotherDay-W/ViewController.swift
@@ -0,0 +1,25 @@
+//
+// ViewController.swift
+// AnotherDay-W
+//
+// Created by jslauren on 2021/03/06.
+// Copyright © 2021 jslauren. All rights reserved.
+//
+// Swift version : 5.0
+// MacOS version : 11.2
+//
+
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
