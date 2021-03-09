141107f144e96d874805364584fe06eeee3a81bb
j.s.lauren <zzzbjs@naver.com>
2021-03-09 17:18:02 +0900

    메인화면 디자인 및 AddLocationVC에서 특정 지역 선택 시, 해당 지역 정보를 메인VC에 띄우도록 셋팅하기.

--- AnotherDay-W/Source/Controller/MainVC.swift
+++ AnotherDay-W/Source/Controller/MainVC.swift
@@ -15,15 +15,29 @@ import SideMenuSwift
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var temperatureImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var maxTemperatureLabel: UILabel!
+    @IBOutlet weak var minTemperatureLabel: UILabel!
+    @IBOutlet weak var compareLabel: UILabel!
+    
+    var weatherLocation: WeatherLocation!
+    var weatherLocations: [WeatherLocation] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // PermissionVC에서 네비게이션 push로 넘어왔기때문에 생성된 "Back" 버튼 숨기기.
-        self.navigationItem.setHidesBackButton(true, animated: false)
+        initView()
         
-        // 네비게이션바 위쪽 여백 추가하기.
-        navigationController?.additionalSafeAreaInsets.top = 10
+        if weatherLocation == nil {
+            weatherLocation = WeatherLocation(name: "현재 위치", latitude: 0.0, longitude: 0.0)
+            weatherLocations.append(weatherLocation)
+        }
+        
+        updateUserInterface()
+        
     }
 
     @IBAction func menuButtonDidClicked(_ sender: Any) {
@@ -39,5 +53,40 @@ class MainVC: UIViewController {
         self.navigationController?.pushViewController(vc, animated: false)
     }
     
+    @IBAction func unwindFromMainVC(segue: UIStoryboardSegue) {
+        let source = segue.source as! AddLocationVC
+        
+        self.weatherLocations = source.weatherLocations
+        self.weatherLocation = self.weatherLocations[source.selectedLocationIndex]
+        
+        updateUserInterface()
+    }
+    
+    func initView() {
+        // PermissionVC에서 네비게이션 push로 넘어왔기때문에 생성된 "Back" 버튼 숨기기.
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        
+        // 네비게이션바 위쪽 여백 추가하기.
+        navigationController?.additionalSafeAreaInsets.top = 10
+        
+        // 뷰 전체 색 및 노치와 네비게이션 바 색 변경.
+        self.view.backgroundColor = hexStringToUIColor(hex: baseColor)
+        self.navigationController?.navigationBar.barTintColor = hexStringToUIColor(hex: baseColor)
+    }
+    
+    func updateUserInterface() {
+        placeLabel.text = weatherLocation.name
+        temperatureLabel.text = "--°"
+        maxTemperatureLabel.text = "최고 : --°"
+        minTemperatureLabel.text = "최저 : --°"
+        compareLabel.text = "어제보다 --° 높음"
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! MainVC
+        
+        destination.weatherLocations = weatherLocations
+    }
+
 }
 
---
+++ AnotherDay-W/Source/Model/Color.swift
@@ -0,0 +1,36 @@
+//
+// Color.swift
+// AnotherDay-W
+//
+// Created by jslauren on 2021/03/09.
+// Copyright © 2021 jslauren. All rights reserved.
+//
+// Swift version : 5.0
+// MacOS version : 11.2
+//
+
+import UIKit
+
+let baseColor = "B0D0FF"
+
+func hexStringToUIColor (hex:String) -> UIColor {
+    var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+    if (cString.hasPrefix("#")) {
+        cString.remove(at: cString.startIndex)
+    }
+
+    if ((cString.count) != 6) {
+        return UIColor.gray
+    }
+
+    var rgbValue:UInt64 = 0
+    Scanner(string: cString).scanHexInt64(&rgbValue)
+
+    return UIColor(
+        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+        alpha: CGFloat(1.0)
+    )
+}
